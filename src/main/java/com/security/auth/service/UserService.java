aefd513ff23923c10654577991948de718f40505
kiwon <kkw-11@naver.com>
2025-07-09 16:14:50 +0900

    feat:회원가입 서비스 예외처리 클래스 수정

--- src/main/java/com/security/auth/service/UserService.java
+++ src/main/java/com/security/auth/service/UserService.java
@@ -2,11 +2,14 @@ package com.security.auth.service;
 
 import com.security.auth.dto.SignupRequest;
 import com.security.auth.entity.User;
+import com.security.auth.exception.exceptions.DuplicateEmailException;
 import com.security.auth.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static com.security.auth.exception.ErrorCode.DUPLICATE_EMAIL;
 
 @Service
 @RequiredArgsConstructor
@@ -18,7 +21,7 @@ public class UserService {
     @Transactional
     public void signup(SignupRequest request) {
         if (userRepository.findByEmail(request.getEmail()).isPresent()) {
-            throw new IllegalArgumentException("이미 가입된 이메일입니다.");
+            throw new DuplicateEmailException();
         }
 
         String encodedPassword = passwordEncoder.encode(request.getPassword());
