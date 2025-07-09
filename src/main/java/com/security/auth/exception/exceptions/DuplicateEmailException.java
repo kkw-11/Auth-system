53cbc6e37714217a8f3e915617e5625365985aab
kiwon <kkw-11@naver.com>
2025-07-09 21:33:15 +0900

    fix:DuplicateEmailException 생성자 버그 수정

--- src/main/java/com/security/auth/exception/exceptions/DuplicateEmailException.java
+++ src/main/java/com/security/auth/exception/exceptions/DuplicateEmailException.java
@@ -1,11 +1,12 @@
 package com.security.auth.exception.exceptions;
 
-import com.security.auth.exception.ErrorCode;
+
+import com.security.auth.dto.common.ResponseCode;
 
 public class DuplicateEmailException extends RuntimeException {
 
     public DuplicateEmailException() {
-        super(ErrorCode.DUPLICATE_EMAIL.getMessage());
+        super(ResponseCode.DUPLICATE_EMAIL.getMessage());
     }
 
     public DuplicateEmailException(String message) {
