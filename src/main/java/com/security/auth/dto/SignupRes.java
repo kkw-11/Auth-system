5ee08831893ce88905591cb83c72f7a56c7e422c
kiwon <kkw-11@naver.com>
2025-07-09 21:10:45 +0900

    feat:signup DTO추가

---
+++ src/main/java/com/security/auth/dto/SignupRes.java
@@ -0,0 +1,12 @@
+package com.security.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class SignupRes {
+    private Long userId;
+}
--- src/main/java/com/security/auth/service/UserService.java
+++ src/main/java/com/security/auth/service/UserService.java
@@ -1,6 +1,7 @@
 package com.security.auth.service;
 
-import com.security.auth.dto.SignupRequest;
+import com.security.auth.dto.SignupReq;
+import com.security.auth.dto.SignupRes;
 import com.security.auth.entity.User;
 import com.security.auth.exception.exceptions.DuplicateEmailException;
 import com.security.auth.repository.UserRepository;
@@ -8,8 +9,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import static com.security.auth.exception.ErrorCode.DUPLICATE_EMAIL;
 
 @Service
 @RequiredArgsConstructor
@@ -19,7 +18,7 @@ public class UserService {
     private final BCryptPasswordEncoder passwordEncoder;
 
     @Transactional
-    public void signup(SignupRequest request) {
+    public SignupRes signup(SignupReq request) {
         if (userRepository.findByEmail(request.getEmail()).isPresent()) {
             throw new DuplicateEmailException();
         }
@@ -31,7 +30,9 @@ public class UserService {
         user.setPassword(encodedPassword);
         user.setNickname(request.getNickname());
 
-        userRepository.save(user);
+        User savedUser = userRepository.save(user);
+
+        return savedUser.getId();
     }
 }
 
