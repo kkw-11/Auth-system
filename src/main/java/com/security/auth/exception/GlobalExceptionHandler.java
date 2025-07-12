66922cea28834a1d2ca0e71d76a82dfca8cf9159
kiwon <kkw-11@naver.com>
2025-07-13 08:37:56 +0900

    chore:error log 출력

--- src/main/java/com/security/auth/exception/GlobalExceptionHandler.java
+++ src/main/java/com/security/auth/exception/GlobalExceptionHandler.java
@@ -3,16 +3,19 @@ package com.security.auth.exception;
 import com.security.auth.dto.common.ApiResponse;
 import com.security.auth.dto.common.ResponseCode;
 import com.security.auth.exception.exceptions.DuplicateEmailException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(DuplicateEmailException.class)
     public ResponseEntity<ApiResponse<Void>> handleDuplicateEmailException(DuplicateEmailException ex) {
+        log.error(ex.getMessage());
         return ResponseEntity
                 .status(ResponseCode.DUPLICATE_EMAIL.getHttpStatus())
                 .body(ApiResponse.error(ResponseCode.DUPLICATE_EMAIL));
@@ -20,6 +23,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ApiResponse<Void>> handleValidationException(MethodArgumentNotValidException ex) {
+        log.error(ex.getMessage());
         return ResponseEntity
                 .badRequest()
                 .body(ApiResponse.error(ResponseCode.INVALID_INPUT));
@@ -27,6 +31,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<Void>> handleGenericException(Exception ex) {
+        log.error(ex.getMessage());
         return ResponseEntity
                 .status(ResponseCode.INTERNAL_SERVER_ERROR.getHttpStatus())
                 .body(ApiResponse.error(ResponseCode.INTERNAL_SERVER_ERROR));
